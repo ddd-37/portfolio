f312dea1639c4b38a0190be5a810d94b1b56cc18
Devon Deason <devon.d.deason@gmail.com>
2020-05-18 12:14:04 -0600

    fix case due to Netlify errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "./../components/layout/Layout"
+import Layout from "./../components/Layout/Layout"
 import Banner from "./../components/banner/Banner"
 
 import AboutSection from "./../components/indexPageComponents/AboutSection/AboutSection"
