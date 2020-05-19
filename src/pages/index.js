74fe852675659afbc170835d346d6d4ba506d29b
Devon Deason <devon.d.deason@gmail.com>
2020-05-19 09:24:02 -0600

    fix bad paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import React from "react"
 import Layout from "./../components/Layout/Layout"
 import Banner from "./../components/banner/Banner"
 
-import AboutSection from "./../components/indexPageComponents/AboutSection/AboutSection"
-import ExperienceSection from "../components/indexPageComponents/ExpereinceSection/ExperienceSection"
+import AboutSection from "./../components/IndexPageComponents/AboutSection/AboutSection"
+import ExperienceSection from "./../components/IndexPageComponents/ExpereinceSection/ExperienceSection"
 import { Helmet } from "react-helmet"
-import ProjectsSection from "../components/IndexPageComponents/ProjectsSection/ProjectsSection"
+import ProjectsSection from "./../components/IndexPageComponents/ProjectsSection/ProjectsSection"
 
 export default () => (
   <Layout>
